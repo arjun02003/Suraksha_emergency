8c494ae45732b58ae9782114d7e77eb4322f87cc
arjun02003 <arjunchowhan94@gmail.com>
2026-08-27 01:34:41 -0700

    Update UserRoutes.js

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,15 +1,6 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 
-const protect = require("../middleware/AuthMiddleware");
-const { updateProfile, getCurrentUser } = require("../controllers/UserController");
-
-router.get('/me', protect, getCurrentUser);
-router.put("/update-profile", protect, updateProfile);
-
-module.exports = router;
-=======
 const protect = require("../middleware/AuthMiddleware");
 const { getMe } = require("../controllers/UserController");
 
@@ -17,4 +8,3 @@ const { getMe } = require("../controllers/UserController");
 router.get("/me", protect, getMe);
 
 module.exports = router;
->>>>>>> origin/main
